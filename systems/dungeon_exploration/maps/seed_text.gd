25d046f0c200e8f10de322a01801491d4cecbf23
bene-labs <62158116+bene-labs@users.noreply.github.com>
2023-12-11 23:58:01 +0100

    fix seed_text

--- systems/dungeon_exploration/maps/seed_text.gd
+++ systems/dungeon_exploration/maps/seed_text.gd
@@ -1,5 +1,6 @@
 extends Label
 
+var is_hovered = false
 var seed := ""
 
 func set_seed(seed_txt : String):
@@ -7,9 +8,21 @@ func set_seed(seed_txt : String):
 	text = "Seed: " + seed
 
 func _input(event):
+	if not is_hovered:
+		return
 	if event is InputEventMouseButton && event.is_double_click():
 		self_modulate = Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1))
 		var current_clipboard = DisplayServer.clipboard_get()
 		if current_clipboard == seed:
 			return
 		DisplayServer.clipboard_set(seed)
+
+
+func _on_mouse_entered():
+	is_hovered = true
+	scale = Vector2(1, 1.1)
+
+
+func _on_mouse_exited():
+	is_hovered = false
+	scale = Vector2.ONE

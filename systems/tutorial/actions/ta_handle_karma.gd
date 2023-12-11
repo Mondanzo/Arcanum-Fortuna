e7801fb566bdea1e4e5b5ebe8e4c0d2b1ead1446
bene-labs <62158116+bene-labs@users.noreply.github.com>
2023-12-11 17:20:42 +0100

    fix tutorial

--- systems/tutorial/actions/ta_handle_karma.gd
+++ systems/tutorial/actions/ta_handle_karma.gd
@@ -3,4 +3,7 @@ extends TutorialAction
 @export var card_battle: CardBattle
 
 func _execute():
-	pass
+	var karma_phase = FriendlyKarmaPhase.new()
+	karma_phase.init(card_battle)
+	karma_phase.execute()
+	await karma_phase.completed
--- systems/tutorial/actions/ta_play_out_turn.gd
+++ systems/tutorial/actions/ta_play_out_turn.gd
@@ -1,11 +1,11 @@
 extends TutorialAction
 
 @export var card_battle: CardBattle
+@export var phases : Array[CombatPhase]
+
 
 func _execute():
-	await card_battle.handle_friendly_attacks()
-	await card_battle.handle_friendly_karma()
-	for i in range(4):
-		if card_battle.gameBoard.get_node("EnemyTiles").get_child(0).get_child(i).get_child_count() > 0:
-			await card_battle.gameBoard.try_move_enemy_card_to_front(i)
-	await card_battle.handle_enemy_attacks()
+	for phase in phases:
+		phase.init(card_battle)
+		phase.execute()
+		await phase.completed

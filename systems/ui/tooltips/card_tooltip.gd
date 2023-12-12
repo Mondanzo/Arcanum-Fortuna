06feb8c6dd369bff4f9e8e6d3434b437e5c3f2aa
Mondanzo <danzo@mondanzo.de>
2023-12-12 11:24:34 +0100

    card tooltips now contain on the screen and don't duplicate keywords

--- systems/ui/tooltips/card_tooltip.gd
+++ systems/ui/tooltips/card_tooltip.gd
@@ -11,7 +11,18 @@ func setup(data : CardData):
 	%CardName.text = data.name
 	%CardDescription.text = data.description
 	%CardArtwork.texture = data.artwork
+	var added_keywords = []
 	for keyword in data.keywords:
-		var keyword_tooltip = sigil_info_template.instantiate()
-		keyword_tooltip.setup(keyword.title, keyword.description, keyword.icon)
-		%Sigils.add_child(keyword_tooltip)
+		if not typeof(keyword) in added_keywords:
+			var keyword_tooltip = sigil_info_template.instantiate()
+			keyword_tooltip.setup(keyword.title, keyword.description, keyword.icon)
+			%Sigils.add_child(keyword_tooltip)
+			added_keywords.append(typeof(keyword))
+
+
+func _process(delta):
+	var target_position = get_global_mouse_position() + mouse_offset
+	var max_size = get_viewport_rect().size - $Container.get_rect().size
+	target_position.x = clampf(target_position.x, 0, max_size.x)
+	target_position.y = clampf(target_position.y, 0, max_size.y)
+	global_position = target_position

7e2478ce48261f66267e595387304ae6c9e2d2c5
Mondanzo <danzo@mondanzo.de>
2023-12-13 11:22:44 +0100

    fixed keywords not showing more than one

--- systems/ui/tooltips/card_tooltip.gd
+++ systems/ui/tooltips/card_tooltip.gd
@@ -13,11 +13,11 @@ func setup(data : CardData):
 	%CardArtwork.texture = data.artwork
 	var added_keywords = []
 	for keyword in data.keywords:
-		if not typeof(keyword) in added_keywords:
+		if not keyword.get_script() in added_keywords:
 			var keyword_tooltip = sigil_info_template.instantiate()
 			keyword_tooltip.setup(keyword.title, keyword.description, keyword.icon)
 			%Sigils.add_child(keyword_tooltip)
-			added_keywords.append(typeof(keyword))
+			added_keywords.append(keyword.get_script())
 
 
 func _process(delta):

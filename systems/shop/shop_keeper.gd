091c7db88bb20a2e174441324311f2b98e8efe5c
Mondanzo <danzo@mondanzo.de>
2023-12-13 11:14:43 +0100

    created basic layout for shop

--- systems/shop/shop_keeper.gd
+++ systems/shop/shop_keeper.gd
@@ -14,23 +14,3 @@ var player_data_ref: PlayerData
 
 func setup(player_data: PlayerData):
 	player_data_ref = player_data
-	for card in player_data.cardStack:
-		var instance = card_template.instantiate() as HandCard
-		instance.init(card.artwork, card.name, card.cost, card.attack, card.health, card.keywords)
-		%Hand.add_child(instance)
-		instance.drag_ended.connect(on_card_dragged)
-
-
-func _process(delta):
-	%DestroyButton.disabled = not (%DestroyPanel.has_card() and player_data_ref.currency >= 10)
-	%Money.text = str(player_data_ref.currency)
-
-func destroy_card():
-	%DestroyPanel.destroy(player_data_ref)
-
-
-func on_card_dragged(card: HandCard):
-	if %DestroyPanel.is_hovered():
-		%DestroyPanel.replace_card(card, %Hand)
-	else:
-		card.reparent(%Hand)

b93d56140503652a00ce735e05a79f4078e9f62b
Scottymaunz <121938128+Scottymaunz@users.noreply.github.com>
2023-12-13 17:47:26 +0100

    Shorten in-battle wait times

--- systems/card_combat/keywords/keyword.gd
+++ systems/card_combat/keywords/keyword.gd
@@ -4,7 +4,7 @@ extends Resource
 @export var title := "Keyword"
 @export_multiline var description := "Gives your card an ability and stuff"
 @export var icon : Texture
-@export var highlight_duration := 1.0
+@export var highlight_duration := 0.5
 var id : int
 
 func init(id = 0):

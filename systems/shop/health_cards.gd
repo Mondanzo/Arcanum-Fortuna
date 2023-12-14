e3734764d07dd21793d65e312d6fe0a86d6ea630
Mondanzo <danzo@mondanzo.de>
2023-12-14 11:42:31 +0100

    added more customization

--- systems/shop/health_cards.gd
+++ systems/shop/health_cards.gd
@@ -1,7 +1,13 @@
 extends Node
 
 @export var card_template: PackedScene
+## Should be at least as many as slots for healing cards available.
+## If more than slots values are used random values will be picked.
 @export var potential_values : Array[HealingValue]
+
+## Should the card be removed after being purchased?
+@export var remove_card_after_purchase := true
+## Should only one card be purchased in total?
 @export var can_only_buy_one := true
 
 
@@ -16,19 +22,16 @@ func _process(delta):
 
 func setup(rng: RandomNumberGenerator, player_data: PlayerData):
 	player_data_ref = player_data
-	var total_count = $Cards.get_child_count()
 	var grabbag = potential_values.duplicate()
 	var chosen_cards = []
-	if len(grabbag) > total_count:
-		for card in range(total_count):
-			var c = grabbag.pop_at(rng.randi_range(0, len(grabbag) - 1))
-			chosen_cards.append(c)
-	else:
-		chosen_cards = grabbag
-		chosen_cards.sort_custom(func(a, b): return a.healing_amount < b.healing_amount)
 	
+	for card in range(min(len(grabbag), $Cards.get_child_count())):
+		var c = grabbag.pop_at(rng.randi_range(0, len(grabbag) - 1))
+		chosen_cards.append(c)
 	
-	for i in range(total_count):
+	chosen_cards.sort_custom(func(a, b): return a.healing_amount < b.healing_amount)
+	
+	for i in range(min(len(chosen_cards), $Cards.get_child_count())):
 		var c = chosen_cards[i]
 		var slot = $Cards.get_child(i)
 		var instance = card_template.instantiate() as HealingCard
@@ -38,6 +41,9 @@ func setup(rng: RandomNumberGenerator, player_data: PlayerData):
 
 
 func card_clicked(health_card: HealingCard):
+	if not health_card.affordable:
+		return
+	
 	player_data_ref.currency -= health_card.costs
 	player_data_ref.health += health_card.healing_amount
 	health_card.queue_free()

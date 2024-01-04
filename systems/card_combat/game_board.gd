5e1c5aeeb1d6fb5dea5c855f4319f097984688b4
Mondanzo <14519085+Mondanzo@users.noreply.github.com>
2024-01-04 12:17:50 +0100

    animation patch

--- systems/card_combat/game_board.gd
+++ systems/card_combat/game_board.gd
@@ -167,7 +167,7 @@ func _on_active_cards_changed(source):
 	for card : CombatCard in active_cards:
 		for i in range(card.keywords.size()):
 			if card.keywords[i] is ActivatedKeyword and card.keywords[i].triggers & 4:
-				await card.keywords[i].trigger(source, card, {"active_cards": active_cards})
+				await card.keywords[i].trigger(source, card, card.get_node("KeyWords").get_child(i), {"active_cards": active_cards})
 				#card.get_node("KeyWords").get_child(i).scale = Vector2(1.2, 1.2)
 				#await get_tree().create_timer(card.keywords[i].highlight_duration).timeout
 				#card.get_node("KeyWords").get_child(i).scale = Vector2.ONE

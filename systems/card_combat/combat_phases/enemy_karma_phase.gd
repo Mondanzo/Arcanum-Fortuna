6945ea7827094e7948f341d40e38e2b07ffd5636
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-01-08 19:25:36 +0100

    fix enemy karma phase

--- systems/card_combat/combat_phases/enemy_karma_phase.gd
+++ systems/card_combat/combat_phases/enemy_karma_phase.gd
@@ -1,6 +1,13 @@
 class_name EnemyKarmaPhase
 extends FriendlyKarmaPhase
 
+static func get_class_name():
+	return "Enemy Karma Phase"
+
+
+func get_corresponding_trigger():
+	return CombatPhaseTrigger.SourcePhases.ENEMY_KARMA
+
 func get_relevant_cards():
 	return combat.gameBoard.get_front_enemies().filter(
 			func(card: Card):

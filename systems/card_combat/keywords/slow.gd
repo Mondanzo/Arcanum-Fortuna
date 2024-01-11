3b344e51e106bb48699c00e8a7fa6e9820aef7b9
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-01-11 21:43:07 +0100

    mod slow
    add attack_on_first_turn param

--- systems/card_combat/keywords/slow.gd
+++ systems/card_combat/keywords/slow.gd
@@ -2,6 +2,7 @@ class_name Slow
 extends Keyword
 
 @export var turns_to_skip = 1
+@export var attack_on_first_turn = true
 var turns_skipped_lookup : Dictionary = {}
 
 
@@ -14,7 +15,7 @@ func init(id = 1):
 
 func get_new_targets(target_offsets, attacker) -> Array[int]:
 	if not turns_skipped_lookup.has(attacker):
-		turns_skipped_lookup[attacker] = 0
+		turns_skipped_lookup[attacker] = turns_to_skip + 1 if attack_on_first_turn else 0
 	turns_skipped_lookup[attacker] += 1
 	if turns_skipped_lookup[attacker] <= turns_to_skip:
 		return []

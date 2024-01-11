d37faaaaf2be5a748c3993b7b5ae996733b6cfa5
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-01-11 20:32:25 +0100

    fix atk_drain, add deathrattle_heal, add companionship atk

--- systems/card_combat/keywords/atk_drain.gd
+++ systems/card_combat/keywords/atk_drain.gd
@@ -5,7 +5,7 @@ extends ActivatedKeyword
 @export var enable_debug_print = false
 
 var base_decription := ""
-
+var granted_buff := 0
 
 func init(id = 4):
 	if description.count('%d') == 1:
@@ -18,22 +18,23 @@ func trigger(source, owner, target, icon_to_animate, params={}):
 	if not target is CombatCard:
 		push_error("Cannot apply ATKDrain. Invalid target ", target, ".")
 	GlobalLog.add_entry("Card '%s' at position %d-%d triggered ATKdrain." % [target.card_data.name, target.tile_coordinate.x, target.tile_coordinate.y])
-	target.attack = target.base_attack
+	target.attack -= granted_buff
+	granted_buff = 0 
 	if enable_debug_print:
 		print("ATK Drain triggered on ", target.card_name)
 	var hit_count = 0
 	for card : CombatCard in params.active_cards:
 		if enable_debug_print:
-			print("Card " + card.card_name + " costs " + str(card.cost))
+			print("Card '" + card.card_name + "' costs " + str(card.cost))
 		if card.cost > 0:
 			hit_count += 1
-			var print_str = str(target.attack)
-			target.attack += attack_gain
+			var print_str = str(granted_buff)
+			granted_buff += attack_gain
 			if enable_debug_print:
-				print(print_str + " + " + str(attack_gain) + " = " + str(target.attack))
+				print(print_str + " + " + str(attack_gain) + " = " + str(granted_buff))
 	if enable_debug_print:
-		print(str(target.base_attack) + " => " + str(target.attack))
-	target.update_texts()
+		print(str(target.attack) + " => " + str(target.attack + granted_buff))
+	target.attack += granted_buff
 	if base_decription.count('%d') < 2:
 		base_decription += " (%d)"
 	description = base_decription % [attack_gain, hit_count]
---
+++ systems/card_combat/keywords/companionship_atk.gd
@@ -0,0 +1,47 @@
+class_name CompanionshipATK
+extends ActivatedKeyword
+
+@export var attack_gain = 1
+
+var buffed_cards : Array[CombatCard] = []
+
+
+func init(id = 4):
+	if title.count('%d') == 1:
+		title = title % attack_gain
+	if description.count('%d') == 1:
+		description = description % attack_gain
+	super.init(id)
+
+
+func get_target(source, owner, combat = null):
+	if combat == null:
+		push_error("Cannot trigger DeathrattleAtk: Combat must be provided!")
+		return
+	var targets : Array[CombatCard] = []
+	var cards = combat.game_board.get_front_enemies() if owner.is_enemy \
+			else combat.game_board.get_friendly_cards()
+	for i in range(cards.size()):
+		if cards[i] != owner:
+			continue
+		if i > 0 && cards[i-1] != null:
+			targets.append(cards[i-1])
+		if i < cards.size() - 1 && cards[i+1] != null:
+			targets.append(cards[i+1])
+		break
+	return targets
+
+
+func trigger(source, owner, target, icon_to_animate, params={}):
+	await super(source, owner, target, icon_to_animate, params)
+	GlobalLog.add_entry("Card '%s' at position %d-%d triggered Companionship ATK." % [owner.card_data.name, owner.tile_coordinate.x, owner.tile_coordinate.y])
+	
+	if owner.health <= 0:
+		print("[Keyword] Card " + str(owner) + " died and removed their Companionship ATK.")
+		for card in buffed_cards:
+			card.attack -= attack_gain
+		return
+	for card : CombatCard in target:
+		if not card in buffed_cards:
+			card.attack += attack_gain
+	buffed_cards = target
